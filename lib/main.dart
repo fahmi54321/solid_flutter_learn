6c8f77ff4e8a57b7a5a769fc877b9cbb3270a633
unknown <fahmiabdulaziz44@gmail.com>
2023-01-04 13:12:29 +0700

    observer pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:solid_flutter_learn/main_page.dart';
+import 'package:solid_flutter_learn/counter_controller.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +20,88 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Factory Method Pattern',
-      home: MainPage(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CounterController _counterController;
+
+  @override
+  void initState() {
+    _counterController = CounterController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _counterController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Streams Demo"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(),
+          Center(
+              child: StreamBuilder<int>(
+                  stream: _counterController.counterStream,
+                  initialData: 0,
+                  builder: (
+                    BuildContext context,
+                    AsyncSnapshot<int> snapshot,
+                  ) {
+                    if (snapshot.hasData) {
+                      debugPrint(
+                          '_MyHomePageState - <receive> <event notification> data: ${snapshot.data}');
+                      return Text(
+                        '${snapshot.data}',
+                        style: Theme.of(context).textTheme.headline4,
+                      );
+                    } else {
+                      return Text(
+                        'Empty data',
+                        style: Theme.of(context).textTheme.headline4,
+                      );
+                    }
+                  })),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  debugPrint('_MyHomePageState - <send> <event trigger>');
+                  _counterController.eventSink.add(Event.decrement);
+                },
+                tooltip: 'Decrement',
+                child: const Icon(Icons.remove),
+              ),
+              FloatingActionButton(
+                onPressed: () {
+                  debugPrint('_MyHomePageState - <send> <event trigger>');
+                  _counterController.eventSink.add(Event.increment);
+                },
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
